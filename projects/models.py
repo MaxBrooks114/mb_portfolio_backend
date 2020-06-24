51ccc1cfaf2b87de04f7c11d37191f1031b03ee0
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2020-06-24 16:44:49 -0400

    project model attributes and display method

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+
+    name = models.CharField(max_length=50)
+    description = models.TextField(max_length=200)
+    repo_link = models.URLField()
+    demo = models.URLField(blank=True)
+    image = models.ImageField(upload_to='photos/%Y/%m/%d')
+    show = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
